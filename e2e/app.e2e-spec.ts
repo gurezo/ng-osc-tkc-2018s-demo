f402295d8c16cff069daf09f315b627ce311700d
gurezo <akihiko.kigure@gmail.com>
2018-02-23 17:13:00 +0900

    test code modified.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -8,22 +8,29 @@ describe('ng-osc-tkc2018s-demo App', () => {
     page = new AppPage();
   });
 
+  let cnt = 0;
   for (let i = 0; i < 100; i++) {
     it('should display welcome message', () => {
       if (i === 0) {
         page.navigateTo();
-        browser.manage().window().setSize(1920, 1400);
+        browser.manage().window().setSize(1920, 1600);
         browser.sleep(2000);
         expect(page.getParagraphText()).toEqual('Welcome to app!');
         browser.ignoreSynchronization = true;
+        browser.waitForAngular();
       }
       browser.sleep(2000);
       browser.get('http://ashiras.xyz/');
+      expect('http://ashiras.xyz/');
       browser.sleep(2000);
       browser.get('https://angular.io/');
+      expect('https://angular.io/');
       browser.sleep(2000);
       browser.get('https://www.ospn.jp/osc2018-spring/');
+      expect('https://www.ospn.jp/osc2018-spring/');
       browser.sleep(2000);
+      cnt++;
+      console.log('cnt = ' + cnt);
     });
     }
 });
