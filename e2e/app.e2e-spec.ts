39174740c0016fc3330a2a90582f7794f6e7da41
gurezo <akihiko.kigure@gmail.com>
2018-02-23 15:55:47 +0900

    test code add.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('ng-osc-tkc2018s-demo App', () => {
   let page: AppPage;
@@ -9,6 +10,17 @@ describe('ng-osc-tkc2018s-demo App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
+    browser.manage().window().setSize(1920, 1080);
+    browser.sleep(10000);
     expect(page.getParagraphText()).toEqual('Welcome to app!');
+    for (let i = 0; i < 1000; i++ ) {
+      browser.sleep(10000);
+      browser.get('http://ashiras.xyz/');
+      browser.sleep(10000);
+      browser.get('https://angular.io/');
+      browser.sleep(10000);
+      browser.get('https://www.ospn.jp/osc2018-spring/');
+      browser.sleep(10000);
+    }
   });
 });
