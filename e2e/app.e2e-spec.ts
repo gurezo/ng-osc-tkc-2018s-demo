06f46936a0fd38c2e92fa0b2f3b801d390bef78c
gurezo <akihiko.kigure@gmail.com>
2018-02-24 10:12:16 +0900

    test code modified.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -8,26 +8,27 @@ describe('ng-osc-tkc2018s-demo App', () => {
     page = new AppPage();
   });
 
+  browser.manage().window().setSize(1920, 1080);
+  browser.ignoreSynchronization = true;
   let cnt = 0;
-  for (let i = 0; i < 100; i++) {
-    it('should display welcome message', () => {
-      if (i === 0) {
-        page.navigateTo();
-        browser.manage().window().setSize(1920, 1600);
-        browser.sleep(2000);
-        expect(page.getParagraphText()).toEqual('Welcome to app!');
-        browser.ignoreSynchronization = true;
-        browser.waitForAngular();
-      }
-      browser.sleep(2000);
+  for (let i = 0; i < 2; i++) {
+    it('should display ashiras HP', () => {
       browser.get('http://ashiras.xyz/');
+      browser.waitForAngular();
       browser.sleep(2000);
-      browser.get('https://angular.io/');
-      browser.sleep(2000);
-      browser.get('https://www.ospn.jp/osc2018-spring/');
-      browser.sleep(2000);
-      cnt++;
-      console.log('cnt = ' + cnt);
     });
+    it('should display angular.io', () => {
+      browser.get('https://angular.io/');
+      browser.waitForAngular();
+      browser.sleep(2000);
+    });
+    it('should display Open Source Conference Tokyo 2018 spring', () => {
+      browser.get('https://www.ospn.jp/osc2018-spring/');
+      browser.waitForAngular();
+      browser.sleep(2000);
+    });
+    cnt++;
+    console.log('cnt = ' + cnt);
+    browser.waitForAngular();
   }
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,12 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getBlockTitle() {
+    return element(by.css('blockTitle')).getText();
+  }
+
+  getHomeText() {
+    return element(by.css('home')).getText();
+  }
 }
